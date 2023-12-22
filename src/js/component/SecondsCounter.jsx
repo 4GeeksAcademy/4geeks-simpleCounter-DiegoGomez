20df327c6ce94c4c295a1b9eaa8292eb73b2595f
Diego Gómez González <93382813+diegogomezgonza@users.noreply.github.com>
2023-12-22 19:02:21 +0100

    Bonus terminado

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -4,28 +4,57 @@ const Seconds = () => {
   //Declaro counter como la variable para mi estado, y setCounter para poder
   //"setearlo", es decir, actualizar la variable
   const [counter, setCounter] = useState(0);
+  //IntervalId y setIntervalID tienen la misma lógica que los dos anteriores, solo que en vez del intervalo como
+  //tal es su id, es una forma de guardar su identificador para más tarde poder hacer el clearInterval.
+  const [intervalId, setIntervalId] = useState(null);
 
-  useEffect(() => {
-    //Creo un interval el cual se ejecuta cada segundo, este interval se añade al estado,
+  const startInterval = () => {
+     //Compruebo si no hay un interval existente.
+    if (!intervalId) {
+      const newInterval = setInterval(() => {
+        //Creo un interval el cual se ejecuta cada segundo, este interval se añade al estado,
     //de forma que lo que se está actualizando es su estado.
-    const interval = setInterval(() => {
-      //prevCounter es el estado previo a la suma del 1 que se hace cada segundo
-      setCounter((prevCounter) => prevCounter + 1);
-    }, 1000);
+        setCounter((prevCounter) => prevCounter + 1);
+      }, 1000);
+      //Para poder continuar la cuenta del interval inicial sin tener que repetirla, guardo el intervalo
+      //con intervalId, de esta forma, cuando su estado cambie a un reset o a un stop, esas acciones sobre 
+      //el mismo id.
+      setIntervalId(newInterval);
+    }
+  };
 
-    //Utilizo clearInterval, una función de JavaScript la cual detiene las repeticiones del interval.
-    //Al utilizar [], especifico que esta función se ejecute después del primer render, es decir, que
-    //cleanInterval está asegurando que la próxima que vez que se cargue, la cuenta se reinicie.
-    return () => clearInterval(interval);
-  }, []);
+  //Para parar el interval, cambio su estado a null, de forma que no tiene ninguna forma
+  //de sumar al valor inicial 0 del counter
+  const stopInterval = () => {
+    clearInterval(intervalId);
+    setIntervalId(null);
+  };
+
+  //Como el valor por defecto del contador es 0, si lo que quiero es reiniciarlo
+  //le cambio su estado a su valor inicial dándole un 0.
+  const resetInterval = () => {
+    stopInterval();
+    setCounter(0);
+  };
 
   return (
-    <div className="d-flex align-items-center">
-      <div className="m-3">
-        <i className="far fa-clock fs-1"></i>
-      </div>
-      <div>
+    <div className="d-flex flex-column align-items-center">
+      <div className="m-3 d-flex flex-direction-column">
+        {/* Reloj y contador */}
+        <i className="far fa-clock fs-1 px-4"></i>
         <h1>{counter}</h1>
+      </div>
+      {/* Botones que ejecutan las funciones */}
+      <div className="d-flex gap-5">
+        <button onClick={stopInterval} className="bg-transparent border-0">
+          <i className="fa-solid fa-pause text-white"></i>
+        </button>
+        <button onClick={startInterval} className="bg-transparent border-0">
+          <i className="fa-solid fa-play text-white"></i>
+        </button>
+        <button onClick={resetInterval} className="bg-transparent border-0">
+          <i className="fa-solid fa-rotate-left text-white"></i>
+        </button>
       </div>
     </div>
   );
